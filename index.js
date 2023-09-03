3f63c2d181e346b4251f271e99c1ef41abf1bb27
Gregory Salinas <gregory.salinas@gmail.com>
2023-09-03 16:02:35 -0500

    fix the loading on the time display it was showing the 24 hr format for a split second

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var today = new Date();
 var date = today.getMonth()+1 + '-' + today.getDate() + '-' + today.getFullYear();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var time = today.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
 
 document.getElementById("dateDisplay").innerHTML = date;
 document.getElementById("timeDisplay").innerHTML = time;
-
 
 setInterval(myTimer, 1000);
 
 function myTimer() {
   const d = new Date();
-  document.getElementById("timeDisplay").innerHTML = d.toLocaleTimeString();
-} 
+  document.getElementById("timeDisplay").innerHTML = d.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+}
