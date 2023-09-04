57c6b23f65360a2bb8426bfb6493d1f99e992b52
Gregory Salinas <gregory.salinas@gmail.com>
2023-09-04 10:47:24 -0500

    add favicon, delete bootstrap code, added JS DOM maniplulation btn

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ function myTimer() {
   const d = new Date();
   document.getElementById("timeDisplay").innerHTML = d.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
 }
+
+const btn = document.getElementById('btn');
+btn.addEventListener('click', () => {
+    const newColor = randomColor();
+    document.body.style.backgroundColor = newColor;
+    btn.innerHTML = newColor;
+})
+
+const randomColor = () => {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  return `rgb(${r}, ${g}, ${b})`;
+}
